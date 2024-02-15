85b415865c17f430caaa84927396a94a9c4d8df9
ralfxdev <lopezalexander1019@gmail.com>
2024-02-15 00:32:51 -0600

    Add project files.

---
+++ decimal-to-float/Program.cs
@@ -0,0 +1,17 @@
+﻿//Escribe un programa que convierta un número decimal a flotante.
+decimal num;
+float resultado;
+
+try
+{
+    Console.WriteLine("Ingrese un decimal:");
+    num = Convert.ToInt32(Console.ReadLine());
+
+    resultado = Convert.ToSingle(num);
+    Console.WriteLine($"Su conversión a flotante es: {resultado}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
---
+++ integer-to-decimal/Program.cs
@@ -0,0 +1,17 @@
+﻿//Escribe un programa que convierta un número entero a decimal.
+int num;
+decimal resultado;
+
+try
+{
+    Console.WriteLine("Ingrese un entero:");
+    num = Convert.ToInt32(Console.ReadLine());
+
+    resultado = Convert.ToDecimal(num);
+    Console.WriteLine($"Su conversión a decimal es: {resultado}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
