a05430fac6aae6552746e66767ae6afd818b0d80
ralfxdev <lopezalexander1019@gmail.com>
2024-02-15 00:47:07 -0600

    character to integer

---
+++ character-to-integer/Program.cs
@@ -0,0 +1,17 @@
+﻿//Escribe un programa que convierta un caracter a entero.
+char character;
+int resultado;
+
+try
+{
+    Console.WriteLine("Ingrese un cáracter:");
+    character = Convert.ToChar(Console.ReadLine()!);
+
+    resultado = Convert.ToInt32(character);
+    Console.WriteLine($"Su conversión a entero es: {resultado}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
---
+++ float-to-integer/Program.cs
@@ -0,0 +1,17 @@
+﻿//Escribe un programa que convierta un número flotante a entero.
+float num;
+int resultado;
+
+try
+{
+    Console.WriteLine("Ingrese un flotante:");
+    num = Convert.ToSingle(Console.ReadLine());
+
+    resultado = Convert.ToInt32(num);
+    Console.WriteLine($"Su conversión a entero es: {resultado}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
