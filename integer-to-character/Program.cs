0178e9c4aafd6eb1e6018c27fe3a9e6e558a3ba1
ralfxdev <lopezalexander1019@gmail.com>
2024-02-15 00:50:48 -0600

    integer to character

---
+++ integer-to-character/Program.cs
@@ -0,0 +1,17 @@
+﻿//Escribe un programa que convierta un entero a caracter.
+int num;
+char resultado;
+
+try
+{
+    Console.WriteLine("Ingrese un entero:");
+    num = Convert.ToInt32(Console.ReadLine()!);
+
+    resultado = Convert.ToChar(num);
+    Console.WriteLine($"Su conversión a cáracter es: {resultado}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
