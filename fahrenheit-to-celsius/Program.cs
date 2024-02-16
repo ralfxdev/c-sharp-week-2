1a3b66c3e170491bef7330fae311be247cdeab5e
ralfxdev <lopezalexander1019@gmail.com>
2024-02-16 00:25:29 -0600

    fahrenheit to celsius

---
+++ fahrenheit-to-celsius/Program.cs
@@ -0,0 +1,16 @@
+﻿//Escribe un programa que calcule la temperatura en grados Celsius a partir de la temperatura en grados Fahrenheit.
+
+try
+{
+    Console.WriteLine("Ingresa la temperatura en grados Fahrenheit:");
+    double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+    // Convertir la temperatura de Fahrenheit a Celsius utilizando la fórmula: C = (F - 32) * 5/9
+    double celsius = (fahrenheit - 32) * 5 / 9;
+
+    Console.WriteLine($"La temperatura en grados Celsius es: {celsius}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
