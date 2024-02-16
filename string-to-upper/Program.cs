f3d26c037a1fb6c1947ef7dca5292adc9356159e
ralfxdev <lopezalexander1019@gmail.com>
2024-02-15 22:23:41 -0600

    string to upper

---
+++ string-to-upper/Program.cs
@@ -0,0 +1,16 @@
+﻿//Escribe un programa que lea una cadena de texto y la imprima en mayúsculas.
+string input, upperCaseString;
+
+try
+{
+    Console.WriteLine("Por favor, ingresa una cadena de texto:");
+    input = Console.ReadLine()!;
+
+    upperCaseString = input.ToUpper();
+    Console.WriteLine($"{upperCaseString}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
