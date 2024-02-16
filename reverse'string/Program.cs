1b356694a847b43a4d225aee2ed6635b4adfc211
ralfxdev <lopezalexander1019@gmail.com>
2024-02-15 22:50:35 -0600

    reverse-string

---
+++ reverse'string/Program.cs
@@ -0,0 +1,19 @@
+﻿//Escribe un programa que lea una cadena de texto y la imprima al revés.
+string input, reversedString;
+
+try
+{
+    Console.WriteLine("Por favor, ingresa una cadena de texto:");
+    input = Console.ReadLine()!;
+
+    char[] charArray = input.ToCharArray();
+    Array.Reverse(charArray);
+    reversedString = new string(charArray);
+
+    Console.WriteLine($"{reversedString}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
