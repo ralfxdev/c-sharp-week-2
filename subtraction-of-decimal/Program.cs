05d18af41c45966403ce2e1d89b793b5c2a93ee1
ralfxdev <lopezalexander1019@gmail.com>
2024-02-15 21:48:46 -0600

    subtraction of decimal

---
+++ subtraction-of-decimal/Program.cs
@@ -0,0 +1,19 @@
+﻿//Escribe un programa que calcule la resta de dos números decimales.
+decimal n1, n2, resultado;
+
+try
+{
+    Console.WriteLine("Ingrese el primer decimal:");
+    n1 = Convert.ToDecimal(Console.ReadLine()!);
+
+    Console.WriteLine("Ingrese el segundo decimal:");
+    n2 = Convert.ToDecimal(Console.ReadLine()!);
+
+    resultado = n1 - n2;
+    Console.WriteLine($"La resta de {n1} + {n2} es: {resultado}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
