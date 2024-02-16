681a65dac8fbfecf7e7fbf5b3793467e071d87a2
ralfxdev <lopezalexander1019@gmail.com>
2024-02-15 21:54:47 -0600

    multiplication of float

---
+++ multiplication-of-float/Program.cs
@@ -0,0 +1,19 @@
+﻿//Escribe un programa que calcule la multiplicación de dos números flotantes.
+float n1, n2, resultado;
+
+try
+{
+    Console.WriteLine("Ingrese el primer flotante:");
+    n1 = Convert.ToSingle(Console.ReadLine()!);
+
+    Console.WriteLine("Ingrese el segundo flotante:");
+    n2 = Convert.ToSingle(Console.ReadLine()!);
+
+    resultado = n1 * n2;
+    Console.WriteLine($"La multiplicación de {n1} * {n2} es: {resultado}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
