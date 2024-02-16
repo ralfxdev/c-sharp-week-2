ee245f4c70e14e6ba70fe828360d760e082b1a64
ralfxdev <lopezalexander1019@gmail.com>
2024-02-16 00:03:42 -0600

    triangle-area

---
+++ triangle-area/Program.cs
@@ -0,0 +1,18 @@
+﻿//Escribe un programa que calcule el área de un triángulo.
+
+try
+{
+    Console.WriteLine("Ingresa la longitud de la base del triángulo:");
+    double baseLength = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Ingresa la altura del triángulo:");
+    double height = Convert.ToDouble(Console.ReadLine());
+
+    double area = (baseLength * height) / 2;
+
+    Console.WriteLine($"El área del triángulo es: {area}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
