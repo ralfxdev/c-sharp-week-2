f53f69ccb00921c743b183ce2615eb23aa4cbdc1
ralfxdev <lopezalexander1019@gmail.com>
2024-02-15 21:59:05 -0600

    division of integer

---
+++ division-of-integer/Program.cs
@@ -0,0 +1,19 @@
+﻿//Escribe un programa que calcule la división de dos números enteros.
+int n1, n2, resultado;
+
+try
+{
+    Console.WriteLine("Ingrese el primer entero:");
+    n1 = Convert.ToInt32(Console.ReadLine()!);
+
+    Console.WriteLine("Ingrese el segundo entero:");
+    n2 = Convert.ToInt32(Console.ReadLine()!);
+
+    resultado = n1 / n2;
+    Console.WriteLine($"La división de {n1} / {n2} es: {resultado}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
