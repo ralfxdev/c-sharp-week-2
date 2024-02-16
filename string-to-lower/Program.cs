9549caa6801c41ed6a393728726796e2c4465d1f
ralfxdev <lopezalexander1019@gmail.com>
2024-02-15 22:26:23 -0600

    string to lower

---
+++ string-to-lower/Program.cs
@@ -0,0 +1,16 @@
+﻿//Escribe un programa que lea una cadena de texto y la imprima en minúsculas.
+string input, lowerCaseString;
+
+try
+{
+    Console.WriteLine("Por favor, ingresa una cadena de texto:");
+    input = Console.ReadLine()!;
+
+    lowerCaseString = input.ToLower();
+    Console.WriteLine($"{lowerCaseString}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
