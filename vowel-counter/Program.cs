aa236fbf7e75b7a041dfa31b4f916b920bb35fdd
ralfxdev <lopezalexander1019@gmail.com>
2024-02-15 23:11:44 -0600

    vowel counter

---
+++ vowel-counter/Program.cs
@@ -0,0 +1,24 @@
+﻿//Escribe un programa que lea una cadena de texto y cuente el número de vocales que contiene.
+string input;
+
+try
+{
+    Console.WriteLine("Por favor, ingresa una cadena de texto:");
+    input = Console.ReadLine()!;
+
+    int vowelCount = 0;
+    foreach (char c in input.ToLower())
+    {
+        if ("aeiou".Contains(c))
+        {
+            vowelCount++;
+        }
+    }
+
+    Console.WriteLine($"El número de vocales en la cadena es: {vowelCount}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
